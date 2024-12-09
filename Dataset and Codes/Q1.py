a3abc4745e2cea394b3e11c933ede862f0a6f79f
Sarfarazzzzz <m.shaik@gwu.edu>
2024-12-09 12:28:49 -0500

    SMART Q1.py

---
+++ Dataset and Codes/Q1.py
@@ -0,0 +1,21 @@
+
+
+
+
+
+data['Improvement_in_Learning_Outcomes'] = data['Improvement_in_Learning_Outcomes'].map({'Yes': 1, 'No': 0})
+
+
+
+# Grouping the data by Instructor VR Proficiency and extracting the 'Improvement_in_Learning_Outcomes' and performing One-Way ANOVA
+
+grouped = [data[data['Instructor_VR_Proficiency'] == proficiency]['Improvement_in_Learning_Outcomes'] 
+           for proficiency in data['Instructor_VR_Proficiency'].unique()]
+
+
+f_statistic, p_value = stats.f_oneway(*grouped)
+
+print(f"F-statistic: {f_statistic}, p-value: {p_value}")
+
+
+# Based on the F-statistic and p-value from the One-Way ANOVA, it seems that Instructor VR Proficiency does not have a statistically significant effect on Improvement in Learning Outcomes.
